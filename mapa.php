c5fce810f7f0cfc98d545fe05b65c0e9752afe02
ElKenshin <ElKenshin@users.noreply.github.com>
2021-05-22 17:49:51 -0500

    Create mapa.php

---
+++ mapa.php
@@ -0,0 +1,11 @@
+<!-- Trae todo el código del header a la página principal -->
+<?php include("includes/header.php") ?>
+
+<!-- Aquí va todo el código propio de la página -->
+    <h1>MAPA</h1>
+    <img src="img/pngtree-cartoon-map-green-map-map-illustration-location-map-png-image_447719.jpg">
+    <h1>Librerías e información</h1>
+    <p>LIBRO LIBRO LIBRO LIBRO LIBRO INFO INFO INFO INFO </p>
+
+<!-- Trae todo el código del footer a la página principal -->
+<?php include("includes/footer.php") ?>
