75b9cb80f654e7d70ead7c77cc79b4363aa9ef12
ProyectoBastilla <proyectobastilla@gmail.com>
2021-06-04 18:54:29 -0500

    Se simplifica el código

--- login/mail.php
+++ login/mail.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 //Inclusión de archivos con info privada
-include("private/correo.php");
+include("private/credenciales.php");
+include("private/correos.php");
 
 //Estas líneas llaman los archivos y clases necesarias para el PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
@@ -9,31 +10,14 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 require("vendor/autoload.php");
 
-//Mensaje como tal en HTML
-$asunto = "Verifica tu cuenta La Bastilla";
-$cuerpo =
-'<html>
-  <head>
-  <style type="text/css">
-    h1 {
-      color: green;
-      font-family: Verdana;
-    }
-  </style>
-  </head>
-  <body>
-    <div>
-      <h1>¡Te has registrado satisfactoriamente!</h1>
-    </div>
-    <div>
-      <h3>El pasaje de librerías La Bastilla te da la bienvenida, este es el lugar donde podrás encontrar la disponibilidad de libros de tus tiendas favoritas a tan solo unos clicks de distancia.<br><br>Para poder usar todas nuestras funcionalidades verifica tu cuenta en el enlace de abajo:</h3>
-    </div>
-    <div>
-      <a href="localhost/login/login_page.php?id=verificacion">Verifica tu cuenta</a>
-    </div>
-  </body>
-</html>'
-;
+//Asignación mensaje del correo. Definidos en "private/correos"
+if ($_GET["type"]=="verifica") {
+  $asunto = $asunto_verifica;
+  $cuerpo = $cuerpo_verifica;
+} elseif ($_GET["type"]=="cambio") {
+  $asunto = $asunto_cambio;
+  $cuerpo = $cuerpo_cambio;
+}
 
 //Crea un objeto del PHPMailer guardado en la variable $mail (POO u OOP)
 $mail = new PHPMailer();
@@ -54,6 +38,7 @@ try {
 
   //Asignar el mensaje que se hizo anteriormente
   $mail->isHTML(true);
+  $mail->CharSet = "UTF-8";
   $mail->Subject = $asunto;
   $mail->Body = $cuerpo;
 
@@ -63,5 +48,5 @@ try {
 } catch (Exception $e) {
   echo "Algo salió mal:".$e->getMessage();
 }
-header("location: index.php");
+//header("location: index.php");
 ?>
