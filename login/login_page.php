13063fcd6b63c556653f148ea893282c252e6f16
ProyectoBastilla <proyectobastilla@gmail.com>
2021-06-04 22:09:37 -0500

    Login full opciones

--- login/login_page.php
+++ login/login_page.php
@@ -6,15 +6,31 @@ if (isset($_SESSION["sesion"])) {
   session_unset();
 }
 include("includes/header.php");
+
+//Asigna el nombre del archivo al que irá el form
+if(isset($_GET["id"])) {
+  $ir = $_GET['id'].'.php';
+} else {
+  $ir = "login.php";
+}
+
+//Asigna el nombre que tendrá el botón para enviar el form
+if (isset($_GET["id"])) {
+
+  if ($_GET["id"]=="verifica") {
+    $enviar = "Verifica tu cuenta";
+  } elseif ($_GET["id"]=="restablecer") {
+    $enviar = "Restablece tu contraseña";
+  } elseif ($_GET["id"]=="edit") {
+    $enviar = "Enviar correo";
+  }
+
+} else {
+  $enviar = "Iniciar Sesión"; //Si no hay nada es un login normal
+}
 ?>
 
 <div id="margenes">
-  <?php if (isset($_GET["id"]) && $_GET["id"]=="verifica") { ?>
-      <div class="container pb-4">
-        <h3>Inicia sesión para completar la verificación de tu cuenta</h3>
-      </div>
-  <?php } ?>
-
   <!-- Mensaje emergente sobre un posible error de login (opcional) -->
   <?php if (isset($_SESSION["mensaje"])) { ?>
     <div class="container alert alert-<?= $_SESSION["mensaje_color"] ?> alert-dismissible fade show" role="alert">
@@ -24,34 +40,66 @@ include("includes/header.php");
     <?php session_unset(); ?>
   <?php } ?>
 
-  <!-- Recuadro específico del login -->
+  <!-- Texto que está arriba del form indicando que hacer -->
   <div id="tarjeta" class="card card-body">
-    <div class="container">
-      <p id="texto_tarjeta">Ingresa tus datos</p>
-    </div>
+    <?php if (isset($_GET["id"]) && $_GET["id"]=="verifica") { ?>
+      <div class="container pb-4">
+        <p id="texto_tarjeta">Inicia sesión para completar la verificación de tu cuenta</p>
+      </div>
+    <?php } elseif (isset($_GET["id"]) && $_GET["id"]=="edit") { ?>
+      <div class="container pb-4">
+        <p id="texto_tarjeta">Ingresa tu correo registrado</p>
+      </div>
+    <?php } elseif (isset($_GET["id"]) && $_GET["id"]=="restablecer") { ?>
+      <div class="container pb-4">
+        <p id="texto_tarjeta">Ingresa tu correo y escribe tu nueva contraseña</p>
+      </div>
+    <?php } else { ?>
+      <div class="container">
+        <p id="texto_tarjeta">Ingresa tus datos</p>
+      </div>
+    <?php } ?>
 
-    <?php
-    if(isset($_GET["id"]) {
-      $ir = "$_GET['id'].'.php'";
-    } else {
-      $ir = "login.php";
-    }
-    ?>
-
+    <!-- Formulario que envía datos a un archivo definido en la línea 11-15 -->
     <form method="post" action="<?= $ir ?>">
-      <div class="form-group pt-4 ">
-        <input class="form-control" type="email" name="correo" placeholder="Correo Electrónico" autofocus>
-      </div>
 
-      <div class="form-group pt-4">
-        <input class="form-control" type="password" name="contra" placeholder="Contraseña">
-      </div>
+      <!-- Si olvidó contraseña, solo un campo para mandar correo de recuperación -->
+      <?php if (isset($_GET["id"]) && $_GET["id"]=="edit"): ?>
+        <div class="form-group pt-4 ">
+          <input class="form-control" type="email" name="correo" placeholder="Correo Electrónico" autofocus>
+        </div>
+        <div class="form-group pt-5">
+          <input class="btn btn-primary" type="submit" name="enviar" value="<?= $enviar ?>" >
+        </div>
+      </form>
+      <?php else: ?>
+        <!-- Esto es para cualquier otro tipo de acción -->
+        <div class="form-group pt-4 ">
+          <input class="form-control" type="email" name="correo" placeholder="Correo Electrónico" autofocus>
+        </div>
 
-      <div class="form-group pt-5">
-        <input class="btn btn-primary" type="submit" name="enviar" value="Iniciar Sesión" >
-      </div>
-    </form>
+        <div class="form-group pt-4">
+          <input class="form-control" type="password" name="contra" placeholder="Contraseña">
+        </div>
+        <!-- Por si hay que restablecer, se agrega otro campo para repetir la contraseña -->
+        <?php if (isset($_GET["id"]) && $_GET["id"]=="restablecer") { ?>
+          <div class="form-group pt-4">
+            <input class="form-control" type="password" name="contra2" placeholder="Repite tu contraseña">
+          </div>
+        <?php } ?>
 
+        <div class="form-group pt-5">
+          <input class="btn btn-primary" type="submit" name="enviar" value="<?= $enviar ?>" >
+        </div>
+      </form>
+      <?php endif; ?>
+
+    <!-- Botón para restablecer contraseña si es un login normal -->
+    <?php if (!isset($_GET["id"])): ?>
+      <div class="container pt-4">
+        <a class="text-dark" href="login_page.php?id=edit">¿Olvidaste tu contraseña?</a>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
 
