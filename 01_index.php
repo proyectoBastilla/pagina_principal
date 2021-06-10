c5df3f494f2c9682117a2a5d4f388e047d0bd736
ProyectoBastilla <proyectobastilla@gmail.com>
2021-06-10 00:38:32 -0500

    Corrección Carrusel Banner

--- 01_index.php
+++ 01_index.php
@@ -9,26 +9,27 @@
 </div>
 <main class="main">
   <div class="containter">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="img/reloj_arena-banner.jpg" alt="First slide">
+          <img src="img/reloj_arena-banner.jpg" class="d-block w-100">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="img/mapita" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="..." alt="Third slide">
+          <img src="img/cuphead-banner.jpg" class="d-block w-100">
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
     <div class="mx-md-4">
       <br><h1><b>¡NUEVOS LANZAMIENTOS!</b></h1>
