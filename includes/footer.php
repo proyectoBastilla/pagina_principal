70bb835bc6341ae3547803d25ca5271d2503fec4
proyectoBastilla <proyectobastilla@gmail.com>
2021-06-14 22:26:56 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,34 @@
 <!-- Final del código, incluye footer y scripts -->
 
-    <footer class="footer">
-      <div class="container">
-      </div>
-    </footer>
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+     
+       <!-- enviar correo -->
+       <a class="btn btn-outline-light btn-floating m-1" href= "#!"  role="button">
+      <i class="far fa-envelope"></i>
+      </a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/la_bastilla_proyecto/" role="button">
+      <i class="fab fa-instagram"></i>
+      </a>
+
+      <!-- acerca de o info -->
+      <a class="btn btn-outline-light btn-floating m-1" href="01_acerca.php" role="button">
+      <i class="far fa-user"></i>
+      </a>
+
+     
+
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  
+</footer>
 
     <!-- Script BOOTSTRAP 5 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
