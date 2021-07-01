728b297b69ca8355ee739f4e2a7b79736f5aef56
ProyectoBastilla <proyectobastilla@gmail.com>
2021-07-01 13:31:25 -0500

    Conexión Libros - DB

--- 01_libros.php
+++ 01_libros.php
@@ -4,93 +4,36 @@
 <?php include("includes/header.php"); ?>
 
 <div class="mx-md-4">
-  <br><h1><b>AQUÍ VAN LOS LIBROS</b></h1></br>
+  <br><h2><b>LIBROS</b></h2></br>
 </div>
 
-<!-- Tarjetas lanzamientos -->
+<div class="mx-md-4">
+  <div class="card" >
+    <div class="mx-sm-4 my-sm-4" >
+      <h3 class="card-text"><b>Te podría interesar...</b></h3>
+      <div class="card-group">
 
-  <!-- Código de tarjeta
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-         <div class="card-body">
-         <h2><p class="card-text">Título</p></h2>
-         <p> Autor </p>
-        </div>
-      </div>
-  -->
-  <div class="mx-md-4">
-        <div class="card" >
-            
-    
-  <div class="mx-sm-4 my-sm-4" >
-  <h2><b><p class="card-text">BÚSQUEDA </p></b></h2>
-  <div class="card-group"> 
-      
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php">   
-        <img class="card-img-top" src="https://www.diariodecultura.com.ar/wp-content/uploads/2020/03/la-peste-albert-camus.jpg" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">La peste</p></h3>
-         <p> Albert Camus </p></a>
-        </div>
-      </div>
+        <?php
+        $query = "SELECT id, titulo, autor, imagen FROM libros";
+        $result = mysqli_query($mysql, $query);
 
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php"> 
-        <img class="card-img-top" src="https://www.librosambigu.com/static/img/portadas/la-revolucion-francesa_60812.jpg" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">La revolución francesa</p></h3>
-         <p> Albert Soboul </p></a>
-        </div>
-      </div>
+        while ($row = mysqli_fetch_array($result)) {
+        ?>
+          <a href="01_descbook.php?id=<?= $row["id"] ?>">
+            <div id="tarjeta_libro" class="card">
+              <img class="card-img-top" src="<?= $row["imagen"] ?>" alt="Card image cap">
+              <div class="card-body">
+                <h4 class="card-text"><?= $row["titulo"] ?></h4>
+                <p><?= $row["autor"] ?></p>
+              </div>
+            </div>
+          </a>
+          <?php } ?>
 
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php"> 
-        <img class="card-img-top" src="https://www.planetadelibros.com.co/usuaris/libros/fotos/164/m_libros/que-tiene-ella-que-no-tenga-yo_9789584232618.jpg" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">¿Qué tiene ella que no tenga yo? </p></h3>
-         <p> Alberto Linero </p></a>
-        </div>
       </div>
-
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php"> 
-        <img class="card-img-top" src="https://http2.mlstatic.com/cuentos-para-quererte-mejor-alex-rovira-D_NQ_NP_677963-MCO41879997884_052020-O.webp" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">Cuentos para quererte mejor</p></h3>
-         <p> Alex Rovira </p></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php"> 
-        <img class="card-img-top" src="https://http2.mlstatic.com/la-ultima-escala-del-tramp-steamer-mutis-el-tiempo-D_NQ_NP_825011-MCO25913425027_082017-F.webp" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">La última escala del Tramp Steamer </p></h3>
-         <p> Alvaro Mutis </p></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-      <a href="01_descbook.php"> 
-        <img class="card-img-top" src="https://lasillarotarm.blob.core.windows.net/images/2018/03/07/cosasquepiensascuandotemuerdeslasunas.jpg" alt="Card image cap">
-         <div class="card-body">
-         <h3><p class="card-text">Cosas que piensas cuando te muerdes las uñas</p></h3>
-         <p> Amalia Andrade </p></a>
-        </div>
-      </div>
-      
-      
-        </div>
-        </div>  
-
-      
-        </div>
-        </div>  
     </div>
+  </div>
 </div>
-</main>
 
 <!-- Trae todo el código del footer a la página principal -->
 <?php include("includes/footer.php"); ?>
-
