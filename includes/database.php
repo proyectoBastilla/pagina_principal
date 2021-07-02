ff7dbec752b9f0c73d87204b9166e1b675fdde1d
ProyectoBastilla <proyectobastilla@gmail.com>
2021-07-01 20:55:52 -0500

    DB Heroku

--- includes/database.php
+++ includes/database.php
@@ -3,10 +3,10 @@
 session_start();
 
 //Info de la base de datos
-$hostname = "localhost";
-$username = "root";
-$pass = "";
-$database = "bastilla";
+$hostname = "pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$username = "uny9vwv876ich7lr";
+$pass = "j5gtrvpb7ygtcrct";
+$database = "j0nh1bs9c8hd9u5o";
 
 //Conexión a la base de datos
 $mysql = mysqli_connect($hostname, $username, $pass, $database);
