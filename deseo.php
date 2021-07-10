96042a1e1795a754eb42ad9bbf8b0c3c3b658b6e
proyectoBastilla <proyectobastilla@gmail.com>
2021-07-10 14:23:10 -0500

    apartado deseo

---
+++ deseo.php
@@ -0,0 +1,20 @@
+<!-- Incluye la sesión y la base de datos -->
+<?php include("includes/database.php"); ?>
+<!-- Trae todo el código del header a la página principal -->
+<?php include("includes/header.php"); ?>
+
+<br></br>
+<div class="card mb-3" style="max-width: 540px; margin-left: 50px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://www.diariodecultura.com.ar/wp-content/uploads/2020/03/la-peste-albert-camus.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Título</h3>
+        <p class="card-text"> Autor</p>
+        <p class="card-text"><small class="text-muted">Libería</small></p>
+      </div>
+    </div>
+  </div>
+</div>
