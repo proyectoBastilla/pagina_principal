778af17179ca12761882a14d337490559a664a96
proyectoBastilla <proyectobastilla@gmail.com>
2021-07-15 11:22:21 -0500

    MAPA

--- mapa.php
+++ mapa.php
@@ -7,7 +7,16 @@
 <div id="margen_general">
     <h1><b>El Pasaje La Bastilla</b></h1><br>
     <div align="center">
-      <img src="img/mapanuevo.jpg" width="80%">
+      <!--Google map-->
+      <div class="card">
+        <div id="map-container-google-12" class="z-depth-1-half map-container-7" style="height: 600px">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d495.764716496399!2d-75.5665046!3d6.2482134!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44285620849f25%3A0xb81bd00a721b3e1!2sCentro%20Comercial%20del%20Libro%20y%20la%20Cultura!5e0!3m2!1ses-419!2sco!4v1626363860754!5m2!1ses-419!2sco" height="77%" width="70%" style="border:0; margin-top: 25px;" allowfullscreen="" loading="lazy"></iframe>
+          </iframe>
+          <br></br>
+          <a href="acerca.php#contacto">
+          <button type="button" class="btn btn-secondary btn-lg btn-block" style="align:center;">Contáctanos</button></a>
+        </div>
+        </div>
       <br></br>
       <p>El Pasaje La Bastilla, ubicado en pleno centro, es un referente histórico de la ciudad. Aquí existió el primer café de Medellín como establecimiento de comercio, del cual el pasaje tomó el nombre. Este café inició en 1920 y era un punto de encuentro de los intelectuales de la época, quienes hacían tertulias al calor de una taza de café o de una copa de licor. Sin embargo, este cerró en 1973, pero se quedó en el recuerdo de muchos.</p>
       <p>Este emblemático Pasaje, ubicado en la carrera 48, una cuadra arriba de Junín, y conformado por tres tramos: de La Playa a Colombia (caracterizado por sus cafés y bares), de Colombia a Ayacucho (reconocido por la venta de libros y donde se encuentra el Centro Comercial del Libro y la Cultura), y de Ayacucho a Pichincha (con sus almacenes de ropa)</p>
