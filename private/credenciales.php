37cb7d9b859906b78dfc27cfb064bb673d37107f
proyectoBastilla <84645446+proyectoBastilla@users.noreply.github.com>
2021-10-03 22:53:04 -0500

    Delete private directory

--- private/credenciales.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Se usan las credenciales en archivo aparte por seguridad
-$miCorreo = "proyectobastilla@gmail.com";
-$key = "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";
-?>
